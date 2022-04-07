4b9d4508f30df51d40f3eba4ff9849597506bd00
Sofia T <91561176+uvsq22104096@users.noreply.github.com>
2022-04-07 17:38:17 +0200

    Update QR-CODE.py

--- QR-CODE.py
+++ QR-CODE.py
@@ -50,6 +50,7 @@ def carre_de_base():
     """Matrice de pixels qui crée un carré noir de 3x3 pixels enrouré d'une bande 
     blanche entourée d'une bande noire entourée d'une bande blanche à droite et en bas"""
     global coin_H_G, coin_B_G, coin_H_D
+    #coin en haut à gauche
     coin_H_G = [[0,0,0,0,0,0,0,1],
                 [0,1,1,1,1,1,0,1],
                 [0,1,0,0,0,1,0,1],
@@ -59,8 +60,8 @@ def carre_de_base():
                 [0,0,0,0,0,0,0,1],
                 [1,1,1,1,1,1,1,1]]
     
-
-    coin_H_D=[[1, 0,0,0,0,0,0,0],
+    #coin en haut à droite
+    coin_H_D=[[1,0,0,0,0,0,0,0],
               [1,0,1,1,1,1,1,0],
               [1,0,1,0,0,0,1,0], 
               [1,0,1,0,0,0,1,0],
@@ -69,10 +70,11 @@ def carre_de_base():
               [1,0,1,1,1,1,1,0],
               [1,0,0,0,0,0,0,0]]
 
-    coin_B_G= [[1,1,1,1,1,1,1,] ,
-              [0,0,0,0,0,0,0, 1], 
+    #coin en bas à guauche
+    coin_B_G=[[1,1,1,1,1,1,1,1],
+              [0,0,0,0,0,0,0,1], 
               [0,1,1,1,1,1,0,1], 
-              [0,1,0,0,0,1,0,1] ,
+              [0,1,0,0,0,1,0,1],
               [0,1,0,0,0,1,0,1], 
               [0,1,0,0,0,1,0,1], 
               [0,1,1,1,1,1,0,1],
@@ -81,43 +83,27 @@ def carre_de_base():
     return coin_H_G , coin_B_G , coin_H_D
 
 
-def trouver_carre():
+def trouver_coin():
     """Fonction qui trouve le coin dans lequel le carre_de_base n'apparait 
     pas et retourne l'image pour avoir les 3 carrés dans les bons coins"""
     global mat
-    for i in range (18, 25):
-        for j in range (18, 25):
-            while mat [i][j] == coin_H_G or mat [i][j] == coin_H_D or mat [i][j] == coin_B_G:
+    coin_B_D = []
+    for i in range (17, 25):
+        for j in range (17, 25):
+            coin_B_D.append(mat[i][j])
+            if coin_B_D == coin_B_G or coin_B_D == coin_H_D or coin_B_D == coin_H_G:
                 rotate_gauche("test_coin.png")
 
 def trouver_lignes():
     """Fonction qui vérifie que les 2 lignes qui relient
     les carrés des 3 coins apparaisent bien """
-    pass
-
-def code_de_Hamming(message): 
-    """Fonction qui en entrée reçoit un message de 4 bits et et qui renvoie 
-    une liste de 7 bits (4bits de message et 3 bits de contrôle)"""
-    #calcul des bits de contrôle
-    c1 = message[0] ^ message[1] ^ message[3]   
-    c2 = message[0] ^ message[2] ^ message[3]
-    c3 = message[1] ^ message[2] ^ message[3]
-    #on positionne les bits de message et de contrôle
-    res = [c1, c2 , message[0], c3, message[1], message[2], message[3]]
-    return res
-
-def decoder_Hamming(bits): 
-    """Fonction qui en entrée a une liste de 7 bits et qui renvoie 
-    les 4 bits d'informations obtenus après correction d'erreur"""
-
-    #calcul des bits de parité
-    p1 = bits[2] ^ bits[4] ^ bits[6] 
-    p2 = bits[2] ^ bits[5] ^ bits[6]
-    p3 = bits[4] ^ bits[5] ^ bits[6]
-    #position de l'erreur s'il y en a une (0 sinon)
-    num = int(p1!=bits[0]) + int(p2 != bits[1])*2 + int(p3!=bits[3])*4 
-
-    if num in [3, 5, 6, 7]: #si la position de l'erreur est sur la position d'un bit de message on corrige
-        bits[num-1] = int(not bits[num-1])  
-        print("Correction d'un pixel corrompu en position " + str(num)+ "\n")
-    return [bits[2], bits[4], bits[5], bits[6]]
+    global mat
+    ligne_horizontale = [0,1,0,1,0,1,0,1,0]
+    ligne_verticale =[[0],[1],[0],[1],[0],[1],[0],[1],[0]]
+    m_H = []
+    m_V = []
+    for i in range(8, 16):
+        m_H.append(mat[6][i])
+        m_V.append(mat[i][6])
+    if m_H != ligne_horizontale or m_V != ligne_verticale:
+        rotate_gauche("test_coin.png")
