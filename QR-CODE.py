8e869e5c3958d8ebf1d50e21864dd4670120b0dd
Sofia T <91561176+uvsq22104096@users.noreply.github.com>
2022-04-22 17:51:35 +0200

    Update QR-CODE.py
    j'ai essayé pleins de trucs différents à chaque fois soit ça ne change pas l'image soit ça me renvoie des erreurs

--- QR-CODE.py
+++ QR-CODE.py
@@ -6,10 +6,17 @@
 # https://github.com/uvsq22104096/Projet_QR_Code.git
 ####################################################
 
+from ast import If
 import tkinter as tk
 import PIL as pil
 from PIL import Image
 from PIL import ImageTk 
+from tkinter import filedialog
+from tkinter import simpledialog
+
+carre = []
+image_courante= "qr_code_ssfiltre_ascii_rotation.png"
+
 
 def nbrCol(matrice):
     """Fonction qui renvoie le nombre de colonnes d'une matrice"""
@@ -39,46 +46,115 @@ def loading(filename):
     return mat
 
 
+def rotate_Droite():
+    mat=loading(image_courante)
+    mat_res = [[0, 0, 0, 0] * nbrLig(mat) for i in range (nbrCol(mat))]
+    for i in range (nbrLig(mat_res)):
+        for j in range (nbrCol(mat_res)):
+            mat_res [i][j] = mat [nbrLig(mat) -1-j][i]
+
 def carre_de_base():
     """Matrice de pixels qui crée un carré noir de 3x3 pixels enrouré d'une bande 
     blanche entourée d'une bande noire entourée d'une bande blanche à droite et en bas"""
+    #global carre
+    """carre = [[0,0,0,0,0,0,0],
+             [0,1,1,1,1,1,0],
+             [0,1,0,0,0,1,0],
+             [0,1,0,0,0,1,0],
+             [0,1,0,0,0,1,0],
+             [0,1,1,1,1,1,0], 
+             [0,0,0,0,0,0,0]]"""
+    carre = [0, 0, 0, 0, 0, 0, 0, 
+             0, 1, 1, 1, 1, 1, 0,
+             0, 1, 0, 0, 0, 1, 0,
+             0, 1, 0, 0, 0, 1, 0,
+             0, 1, 0, 0, 0, 1, 0,
+             0, 1, 1, 1, 1, 1, 0,
+             0, 0, 0, 0, 0, 0, 0]
+    #return carre
 
-    pixels = [[0,0,0,0,0,0,0,1], [0,1,1,1,1,1,0,1], [0,1,0,0,0,1,0,1], [0,1,0,0,0,1,0,1], [0,1,0,0,0,1,0,1], [0,1,1,1,1,1,0,1], [0,0,0,0,0,0,0,1], [1,1,1,1,1,1,1,1]]
-    return pixels
 
-def trouver_carre():
+def trouver_coin():
     """Fonction qui trouve le coin dans lequel le carre_de_base n'apparait 
     pas et retourne l'image pour avoir les 3 carrés dans les bons coins"""
-    pass
+    carre = [0, 0, 0, 0, 0, 0, 0, 
+             0, 1, 1, 1, 1, 1, 0,
+             0, 1, 0, 0, 0, 1, 0,
+             0, 1, 0, 0, 0, 1, 0,
+             0, 1, 0, 0, 0, 1, 0,
+             0, 1, 1, 1, 1, 1, 0,
+             0, 0, 0, 0, 0, 0, 0]
+    mat = loading(image_courante)
+    coin_B_D = []
+    for i in range (18, 25):
+        for j in range (18, 25):
+            coin_B_D.append(mat[i][j])
+    print(coin_B_D)
+    if coin_B_D == carre:
+        rotate_Droite()
+    saving(mat, "photo.png")
+    #return matrice
+    
+    
 
 def trouver_lignes():
     """Fonction qui vérifie que les 2 lignes qui relient
     les carrés des 3 coins apparaisent bien """
-    pass
+    mat = loading(image_courante)
+    ligne_horizontale = [0,1,0,1,0,1,0,1,0]
+    ligne_verticale =[[0],[1],[0],[1],[0],[1],[0],[1],[0]]
+    m_H = []
+    m_V = []
+    for i in range(8, 16):
+        m_H.append(mat[6][i])
+        m_V.append(mat[i][6])
+    if m_H != ligne_horizontale or m_V != ligne_verticale:
+        rotate_Droite()
+        saving(mat, "image.png")
 
-def code_de_Hamming(message): 
-    """Fonction qui en entrée reçoit un message de 4 bits et et qui renvoie 
-    une liste de 7 bits (4bits de message et 3 bits de contrôle)"""
-    #calcul des bits de contrôle
-    c1 = message[0] ^ message[1] ^ message[3]   
-    c2 = message[0] ^ message[2] ^ message[3]
-    c3 = message[1] ^ message[2] ^ message[3]
-    #on positionne les bits de message et de contrôle
-    res = [c1, c2 , message[0], c3, message[1], message[2], message[3]]
-    return res
+def lecture_bloc(mat):  
+    """ Fonction qui parcourt l’image d’un QR code pour renvoyer 
+    l’information lue sous la forme d’une liste de listes de 14 bits"""
 
-def decoder_Hamming(bits): 
-    """Fonction qui en entrée a une liste de 7 bits et qui renvoie 
-    les 4 bits d'informations obtenus après correction d'erreur"""
+    grande_liste=[]   #liste contenant les sous listes de 14 bits
+    bloc = []         # liste qui va contenir 14 bits
+    cpt=0            
+    i=24             # initialiser a 24 pour démarrer la lecture en bas a droite 
+    j=24 
+    bit = mat [i][j]           # ajout du premier bit de lecture en bas a droite dans laa liste de 14 bits
+    bloc.append(bit)
 
-    #calcul des bits de parité
-    p1 = bits[2] ^ bits[4] ^ bits[6] 
-    p2 = bits[2] ^ bits[5] ^ bits[6]
-    p3 = bits[4] ^ bits[5] ^ bits[6]
-    #position de l'erreur s'il y en a une (0 sinon)
-    num = int(p1!=bits[0]) + int(p2 != bits[1])*2 + int(p3!=bits[3])*4 
+#pour le premier bloc  
+    while cpt <14:                # ajout des 13 prochains bits de ma sous liste de 14 bits 
+        cpt+=1 
+        i=i-1                     # mouvement en haut 
+        bit= mat [i][j] 
+        bloc.append(bit)
+   
+        cpt+=1
+        i=i+1                     # mouvment en bas 
+        j=j-1                     # puis mouvement a gauche
+        bit = mat [i][j]
+        bloc.append(bit)   
+    grande_liste.append(bloc)             # ajout de sous liste de 14 bits = une bloc
+    bloc = []
 
-    if num in [3, 5, 6, 7]: #si la position de l'erreur est sur la position d'un bit de message on corrige
-        bits[num-1] = int(not bits[num-1])  
-        print("Correction d'un pixel corrompu en position " + str(num)+ "\n")
-    return [bits[2], bits[4], bits[5], bits[6]]
+#pour le deuxième bloc
+    while 14 < cpt < 28:    
+        """i -= 1    # pour pouvoir lire le 1er bit
+        bloc.append(mat[i][j])
+        i -= 1    #pour pouvoir lire le 2e bit
+        bloc.append(mat[i][j])
+        cpt+=1"""       #problème ça va se refaire à chaque fois #demander quand on monte de 2 pixels si on monte ou on descend
+        i=i+1                     # mouvment en bas 
+        j=j+1    # puis mouvement a droite
+        bit= mat [i][j] 
+        bloc.append(bit)
+    grande_liste.append(bloc)
+    cpt = 0
+
+
+
+# programme principal
+print(trouver_coin())
+#print(trouver_lignes())
